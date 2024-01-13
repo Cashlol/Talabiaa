0c5ca637a0a85bc44524b27192be3ce19827c08a
0JingYing <114901307+0JingYing@users.noreply.github.com>
2024-01-13 23:14:30 +0800

    Update Time.java

--- src/Model/Piece/Time.java
+++ src/Model/Piece/Time.java
@@ -12,6 +12,38 @@ public class Time extends Piece{
         image = getImage((color==0) ? "/res/b-time" : "/res/y-time");
     }
 
+    @Override
+    public boolean canMove(int targetCol, int targetRow) {
+        // Check if the new position is a valid diagonal move
+        int colDiff = Math.abs(targetCol - getCol());
+        int rowDiff = Math.abs(targetRow - getRow());
+    
+        if (colDiff == rowDiff) {
+            // Check if there are no pieces in the diagonal path
+            int colIncrement = (targetCol > getCol()) ? 1 : -1;
+            int rowIncrement = (targetRow > getRow()) ? 1 : -1;
+    
+            int currentCol = getCol() + colIncrement;
+            int currentRow = getRow() + rowIncrement;
+    
+            while (currentCol != targetCol && currentRow != targetRow) {
+                if (board.getPieceArray()[currentRow][currentCol] != null) {
+                    // There is a piece in the path, cannot move
+                    return false;
+                }
+            
+                currentCol += colIncrement;
+                currentRow += rowIncrement;
+            }
+    
+            // No pieces in the path, valid move
+            return true;
+        }
+    
+        return false;
+    }
+    
+
     //display the piece symbol
     //blue = 0, uppercase symbol
     //yellow = 1, lowercase symbol
