84b7b012745c0a3e5fc0d8cf97a6952c101505f9
0JingYing <114901307+0JingYing@users.noreply.github.com>
2024-01-13 23:13:51 +0800

    Update Plus.java

--- src/Model/Piece/Plus.java
+++ src/Model/Piece/Plus.java
@@ -1,55 +1,54 @@
 package Model.Piece;
 
-public class Plus extends Piece{
+public class Time extends Piece{
 
     private int color;
     
     //constuctor to pass color, column and row into super class
-    public Plus(int color, int col, int row) {
+    public Time(int color, int col, int row) {
         super(color, col, row);
         this.color=color;
         //check which color is the piece and return their respective imagepath
-        image = getImage((color==0) ? "/res/b-plus" : "/res/y-plus");
-        
+        image = getImage((color==0) ? "/res/b-time" : "/res/y-time");
     }
 
     @Override
-public boolean canMove(int targetCol, int targetRow) {
-    // Check if the new position is a valid horizontal or vertical move
-    int colDiff = Math.abs(targetCol - getCol());
-    int rowDiff = Math.abs(targetRow - getRow());
-
-    if ((colDiff == 0 && rowDiff > 0) || (colDiff > 0 && rowDiff == 0)) {
-        // Check if there are no pieces in the path
-        int colIncrement = (targetCol > getCol()) ? 1 : (targetCol < getCol()) ? -1 : 0;
-        int rowIncrement = (targetRow > getRow()) ? 1 : (targetRow < getRow()) ? -1 : 0;
-
-        int currentCol = getCol() + colIncrement;
-        int currentRow = getRow() + rowIncrement;
-
-        while (currentCol != targetCol || currentRow != targetRow) {
-            if (board.getPieceArray()[currentRow][currentCol] != null) {
-                // There is a piece in the path, cannot move
-                return false;
+    public boolean canMove(int targetCol, int targetRow) {
+        // Check if the new position is a valid diagonal move
+        int colDiff = Math.abs(targetCol - getCol());
+        int rowDiff = Math.abs(targetRow - getRow());
+    
+        if (colDiff == rowDiff) {
+            // Check if there are no pieces in the diagonal path
+            int colIncrement = (targetCol > getCol()) ? 1 : -1;
+            int rowIncrement = (targetRow > getRow()) ? 1 : -1;
+    
+            int currentCol = getCol() + colIncrement;
+            int currentRow = getRow() + rowIncrement;
+    
+            while (currentCol != targetCol && currentRow != targetRow) {
+                if (board.getPieceArray()[currentRow][currentCol] != null) {
+                    // There is a piece in the path, cannot move
+                    return false;
+                }
+            
+                currentCol += colIncrement;
+                currentRow += rowIncrement;
             }
-
-            currentCol += colIncrement;
-            currentRow += rowIncrement;
+    
+            // No pieces in the path, valid move
+            return true;
         }
-
-        // No pieces in the path, valid move
-        return true;
+    
+        return false;
     }
-
-    return false;
-}
-
+    
 
     //display the piece symbol
     //blue = 0, uppercase symbol
     //yellow = 1, lowercase symbol
     @Override
     public String toString(){
-        return (color==0) ? "PL" : "pl";
+        return (color==0) ? "TM" : "tm";
     }
 }
