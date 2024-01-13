a86c5512bbf3ae1a410a6f3ccff9b0e15bceab6d
0JingYing <114901307+0JingYing@users.noreply.github.com>
2024-01-13 23:13:42 +0800

    Update Plus.java

--- src/Model/Piece/Plus.java
+++ src/Model/Piece/Plus.java
@@ -13,6 +13,38 @@ public class Plus extends Piece{
         
     }
 
+    @Override
+public boolean canMove(int targetCol, int targetRow) {
+    // Check if the new position is a valid horizontal or vertical move
+    int colDiff = Math.abs(targetCol - getCol());
+    int rowDiff = Math.abs(targetRow - getRow());
+
+    if ((colDiff == 0 && rowDiff > 0) || (colDiff > 0 && rowDiff == 0)) {
+        // Check if there are no pieces in the path
+        int colIncrement = (targetCol > getCol()) ? 1 : (targetCol < getCol()) ? -1 : 0;
+        int rowIncrement = (targetRow > getRow()) ? 1 : (targetRow < getRow()) ? -1 : 0;
+
+        int currentCol = getCol() + colIncrement;
+        int currentRow = getRow() + rowIncrement;
+
+        while (currentCol != targetCol || currentRow != targetRow) {
+            if (board.getPieceArray()[currentRow][currentCol] != null) {
+                // There is a piece in the path, cannot move
+                return false;
+            }
+
+            currentCol += colIncrement;
+            currentRow += rowIncrement;
+        }
+
+        // No pieces in the path, valid move
+        return true;
+    }
+
+    return false;
+}
+
+
     //display the piece symbol
     //blue = 0, uppercase symbol
     //yellow = 1, lowercase symbol
